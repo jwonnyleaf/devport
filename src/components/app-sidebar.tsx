96984b1c1e958069656ac4adff765c40af39ef12
Johnny Le <jwonnyleaf@gmail.com>
2025-03-16 00:15:36 -0500

    feat: something

---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,44 @@
+'use client';
+
+import { motion } from 'framer-motion';
+import { Github, Instagram, Linkedin } from 'lucide-react';
+
+export const AppSidebar = () => {
+  return (
+    <div className="w-[48%] h-screen py-24">
+      <div className="h-full flex flex-col justify-between">
+        {/* Sidebar Header */}
+        <header>
+          <h1 className="text-5xl font-bold text-primary">
+            <a>Johnny Le</a>
+          </h1>
+          <h2 className="mt-3 text-xl text-primary">
+            Full Stack Software Engineer
+          </h2>
+        </header>
+        {/* Social Links */}
+        <ul className="flex gap-6 ml-4 text-primary-foreground">
+          {[
+            { href: '#', Icon: Github },
+            { href: '#', Icon: Linkedin },
+            { href: '#', Icon: Instagram },
+          ].map(({ href, Icon }, index) => (
+            <motion.div
+              key={index}
+              whileHover={{
+                scale: 1.4,
+                rotate: Math.random() > 0.5 ? 5 : -5,
+              }}
+              whileTap={{ scale: 0.9 }}
+              transition={{ type: 'spring', stiffness: 300 }}
+            >
+              <a href={href} target="_blank" className="hover:text-primary">
+                <Icon size={32} />
+              </a>
+            </motion.div>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
