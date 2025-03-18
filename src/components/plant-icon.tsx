cabf977fcb12e76cabf7621fdccf0d887732b2f9
Johnny Le <jwonnyleaf@gmail.com>
2025-03-18 02:31:59 -0500

    fix: mobile disappearing plant

--- src/components/plant-icon.tsx
+++ src/components/plant-icon.tsx
@@ -1,30 +1,33 @@
-import { motion } from 'framer-motion';
+import { motion, useAnimation } from 'framer-motion';
 import Image from 'next/image';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 
 export const PlantIcon = () => {
-  const [animationKey, setAnimationKey] = useState(0);
+  const controls = useAnimation();
 
   useEffect(() => {
-    const randomDelay = Math.random() * 5000 + 1000;
-    const interval = setInterval(() => {
-      setAnimationKey((prev) => prev + 1);
-    }, randomDelay);
+    let timeoutId: NodeJS.Timeout;
 
-    return () => clearInterval(interval);
-  }, []);
+    const triggerAnimation = () => {
+      controls.start({
+        rotate: [0, Math.random() * 10 - 5, Math.random() * 10 - 5, 0],
+        transition: {
+          duration: 3 + Math.random() * 2,
+          ease: 'easeInOut',
+        },
+      });
+
+      const randomDelay = Math.random() * 7000 + 1000;
+      timeoutId = setTimeout(triggerAnimation, randomDelay);
+    };
+
+    triggerAnimation();
+
+    return () => clearTimeout(timeoutId);
+  }, [controls]);
 
   return (
-    <motion.div
-      key={animationKey}
-      animate={{
-        rotate: [0, Math.random() * 20 - 10, Math.random() * 20 - 10, 0],
-      }}
-      transition={{
-        duration: 2.5 + Math.random() * 1.5,
-        ease: 'easeInOut',
-      }}
-    >
+    <motion.div animate={controls}>
       <Image
         src={'/images/logo-white.png'}
         alt="Plant Icon"
